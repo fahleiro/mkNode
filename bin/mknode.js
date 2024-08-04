87d3a544d8582a00525d0ef558d0ae647abd4bfd
Gabriel Reinisch Faleiro <gabriel.faleiro@dbserver.com.br>
2024-08-04 20:30:07 -0300

    mknode fracted

--- bin/mknode.js
+++ bin/mknode.js
@@ -10,6 +10,9 @@ const process = require('process');
 const initializeProject = require('../lib/initializeProject');
 const getFontConfig = require('../lib/getFontConfig');
 const readMenuConfig = require('../lib/readMenuConfig');
+const generateMenuHtml = require('../lib/generateMenuHtml');
+const styles = require('../lib/styles'); // Importa o CSS
+const scripts = require('../lib/scripts'); // Importa o JavaScript
 
 const app = express();
 const port = 3000;
@@ -17,31 +20,7 @@ const docsPath = path.resolve(process.cwd(), 'docs');
 const font = getFontConfig();
 const menuConfig = readMenuConfig();
 
-// Função para gerar o menu HTML
-function generateMenuHtml(menu) {
-  function buildMenu(items, depth = 0) {
-    let html = '<ul>';
-    for (const [key, value] of Object.entries(items)) {
-      if (typeof value === 'object') {
-        html += `
-          <li class="menu-item">
-            <span class="menu-toggle" data-depth="${depth}" data-fulltext="${key}">${key}</span>
-            ${buildMenu(value, depth + 1)}
-          </li>
-        `;
-      } else {
-        html += `
-          <li>
-            <a href="/${value}">${key}</a>
-          </li>
-        `;
-      }
-    }
-    html += '</ul>';
-    return html;
-  }
-  return buildMenu(menu);
-}
+
 
 // Middleware para converter e servir arquivos Markdown
 app.get('*.md', (req, res) => {
@@ -58,87 +37,7 @@ app.get('*.md', (req, res) => {
       <html>
       <head>
         <style>
-          body { font-family: ${font}; margin: 0; padding: 0; }
-          nav {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 200px;
-            height: 100%;
-            background-color: #6482AD;
-            color: white;
-            padding: 20px;
-            box-sizing: border-box;
-            overflow-y: auto; /* Adiciona rolagem vertical */
-          }
-          /* Estilo da barra de rolagem */
-          nav::-webkit-scrollbar {
-            width: 8px;
-          }
-          nav::-webkit-scrollbar-track {
-            background: #f1f1f1;
-          }
-          nav::-webkit-scrollbar-thumb {
-            background: #888;
-            border-radius: 10px;
-          }
-          nav::-webkit-scrollbar-thumb:hover {
-            background: #555;
-          }
-          nav ul {
-            list-style-type: none;
-            padding: 0;
-            margin: 0;
-          }
-          nav ul li {
-            margin: 10px 0;
-            position: relative;
-          }
-          nav ul li ul {
-            display: none;
-            padding-left: 20px;
-          }
-          nav ul li.active > ul {
-            display: block;
-          }
-          nav ul li a {
-            color: white;
-            text-decoration: none;
-            display: block;
-            padding: 5px;
-          }
-          nav ul li a:hover {
-            background-color: #4a6a91;
-            border-radius: 5px;
-          }
-          .menu-toggle {
-            cursor: pointer;
-            display: block;
-            padding: 5px;
-            background-color: #4a6a91;
-            border-radius: 5px;
-            margin: 0;
-            overflow: hidden; /* Impede o texto de ultrapassar o contêiner */
-            text-overflow: ellipsis; /* Adiciona '...' no final do texto cortado */
-            white-space: nowrap; /* Impede que o texto quebre em várias linhas */
-          }
-          .menu-toggle:hover::after {
-            content: attr(data-fulltext); /* Mostra o texto completo no tooltip */
-            position: absolute;
-            left: 100%;
-            white-space: nowrap;
-            background-color: #333;
-            color: #fff;
-            padding: 5px;
-            border-radius: 5px;
-            box-shadow: 0 0 10px rgba(0,0,0,0.5);
-            z-index: 1000;
-            display: block;
-          }
-          .content {
-            margin-left: 220px;
-            padding: 20px;
-          }
+          ${styles}
         </style>
       </head>
       <body>
@@ -149,16 +48,7 @@ app.get('*.md', (req, res) => {
           ${html}
         </div>
         <script>
-          document.addEventListener('DOMContentLoaded', () => {
-            const toggles = document.querySelectorAll('.menu-toggle');
-            toggles.forEach(toggle => {
-              toggle.setAttribute('data-fulltext', toggle.textContent);
-              toggle.addEventListener('click', () => {
-                const parentLi = toggle.parentElement;
-                parentLi.classList.toggle('active');
-              });
-            });
-          });
+          ${scripts}
         </script>
       </body>
       </html>
@@ -182,87 +72,7 @@ app.get('/', (req, res) => {
       <html>
       <head>
         <style>
-          body { font-family: ${font}; margin: 0; padding: 0; }
-          nav {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 200px;
-            height: 100%;
-            background-color: #6482AD;
-            color: white;
-            padding: 20px;
-            box-sizing: border-box;
-            overflow-y: auto; /* Adiciona rolagem vertical */
-          }
-          /* Estilo da barra de rolagem */
-          nav::-webkit-scrollbar {
-            width: 8px;
-          }
-          nav::-webkit-scrollbar-track {
-            background: #f1f1f1;
-          }
-          nav::-webkit-scrollbar-thumb {
-            background: #888;
-            border-radius: 10px;
-          }
-          nav::-webkit-scrollbar-thumb:hover {
-            background: #555;
-          }
-          nav ul {
-            list-style-type: none;
-            padding: 0;
-            margin: 0;
-          }
-          nav ul li {
-            margin: 10px 0;
-            position: relative;
-          }
-          nav ul li ul {
-            display: none;
-            padding-left: 20px;
-          }
-          nav ul li.active > ul {
-            display: block;
-          }
-          nav ul li a {
-            color: white;
-            text-decoration: none;
-            display: block;
-            padding: 5px;
-          }
-          nav ul li a:hover {
-            background-color: #4a6a91;
-            border-radius: 5px;
-          }
-          .menu-toggle {
-            cursor: pointer;
-            display: block;
-            padding: 5px;
-            background-color: #4a6a91;
-            border-radius: 5px;
-            margin: 0;
-            overflow: hidden; /* Impede o texto de ultrapassar o contêiner */
-            text-overflow: ellipsis; /* Adiciona '...' no final do texto cortado */
-            white-space: nowrap; /* Impede que o texto quebre em várias linhas */
-          }
-          .menu-toggle:hover::after {
-            content: attr(data-fulltext); /* Mostra o texto completo no tooltip */
-            position: absolute;
-            left: 100%;
-            white-space: nowrap;
-            background-color: #333;
-            color: #fff;
-            padding: 5px;
-            border-radius: 5px;
-            box-shadow: 0 0 10px rgba(0,0,0,0.5);
-            z-index: 1000;
-            display: block;
-          }
-          .content {
-            margin-left: 220px;
-            padding: 20px;
-          }
+          ${styles}
         </style>
       </head>
       <body>
@@ -273,16 +83,7 @@ app.get('/', (req, res) => {
           ${html}
         </div>
         <script>
-          document.addEventListener('DOMContentLoaded', () => {
-            const toggles = document.querySelectorAll('.menu-toggle');
-            toggles.forEach(toggle => {
-              toggle.setAttribute('data-fulltext', toggle.textContent);
-              toggle.addEventListener('click', () => {
-                const parentLi = toggle.parentElement;
-                parentLi.classList.toggle('active');
-              });
-            });
-          });
+          ${scripts}
         </script>
       </body>
       </html>
